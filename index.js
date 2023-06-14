dc51659017ec21e9e5657c2ba1ea802fde03b3a5
Alexandra Bichir <alexandrabichir@yahoo.com>
2023-06-14 21:27:53 +0300

    add showHome and showSkills functions

---
+++ index.js
@@ -0,0 +1,12 @@
+function showHome() {
+  var page = document.getElementById("home");
+  //console.info("page", page);
+  page.style.display = "block";
+}
+
+function showSkills() {
+  var page = document.getElementById("skills");
+  page.style.display = "block";
+}
+
+showHome();
