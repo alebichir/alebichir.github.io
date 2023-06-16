b15704d0b54c85a7a86359d16ed6252b8a5e209d
Alexandra Bichir <alexandrabichir@yahoo.com>
2023-06-16 19:09:37 +0300

    hide all pages before showing other pages

--- index.js
+++ index.js
@@ -1,53 +1,88 @@
+function hideAllPages() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+}
+
 function showHome() {
+  hideAllPages();
   var page = document.getElementById("home");
   page.style.display = "block";
-  hideSkills();
-  hideLanguages();
-  hideProjects();
 }
 
 function showSkills() {
+  hideAllPages();
   var page = document.getElementById("skills");
   page.style.display = "block";
-  hideHome();
-  hideLanguages();
-  hideProjects();
 }
 
 function showLanguages() {
+  hideAllPages();
   var page = document.getElementById("languages");
   page.style.display = "block";
-  hideHome();
-  hideSkills();
-  hideProjects();
 }
 
 function showProjects() {
+  hideAllPages();
   var page = document.getElementById("projects");
   page.style.display = "block";
-  hideHome();
-  hideSkills();
-  hideLanguages();
 }
 
-function hideHome() {
-  var page = document.getElementById("home");
-  page.style.display = "none";
-}
+//debugger;
 
-function hideSkills() {
-  var page = document.getElementById("skills");
-  page.style.display = "none";
-}
+showHome();
 
-function hideLanguages() {
-  var page = document.getElementById("languages");
-  page.style.display = "none";
-}
+// function showHome() {
+//   var page = document.getElementById("home");
+//   page.style.display = "block";
+//   hideSkills();
+//   hideLanguages();
+//   hideProjects();
+// }
 
-function hideProjects() {
-  var page = document.getElementById("projects");
-  page.style.display = "none";
-}
+// function showSkills() {
+//   var page = document.getElementById("skills");
+//   page.style.display = "block";
+//   hideHome();
+//   hideLanguages();
+//   hideProjects();
+// }
+
+// function showLanguages() {
+//   var page = document.getElementById("languages");
+//   page.style.display = "block";
+//   hideHome();
+//   hideSkills();
+//   hideProjects();
+// }
+
+// function showProjects() {
+//   var page = document.getElementById("projects");
+//   page.style.display = "block";
+//   hideHome();
+//   hideSkills();
+//   hideLanguages();
+// }
+
+// function hideHome() {
+//   var page = document.getElementById("home");
+//   page.style.display = "none";
+// }
+
+// function hideSkills() {
+//   var page = document.getElementById("skills");
+//   page.style.display = "none";
+// }
+
+// function hideLanguages() {
+//   var page = document.getElementById("languages");
+//   page.style.display = "none";
+// }
+
+// function hideProjects() {
+//   var page = document.getElementById("projects");
+//   page.style.display = "none";
+// }
 
 showHome();
