e02e15bccc5beb48df1ceb556dbc3ddf4198630b
Alexandra Bichir <alexandrabichir@yahoo.com>
2023-06-16 18:35:38 +0300

    add showProjects, showLanguages, hideHome, hideLangages, hideSkills, hideLanguages, hideProjects

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
 function showHome() {
   var page = document.getElementById("home");
-  //console.info("page", page);
   page.style.display = "block";
+  hideSkills();
+  hideLanguages();
+  hideProjects();
 }
 
 function showSkills() {
   var page = document.getElementById("skills");
   page.style.display = "block";
+  hideHome();
+  hideLanguages();
+  hideProjects();
+}
+
+function showLanguages() {
+  var page = document.getElementById("languages");
+  page.style.display = "block";
+  hideHome();
+  hideSkills();
+  hideProjects();
+}
+
+function showProjects() {
+  var page = document.getElementById("projects");
+  page.style.display = "block";
+  hideHome();
+  hideSkills();
+  hideLanguages();
+}
+
+function hideHome() {
+  var page = document.getElementById("home");
+  page.style.display = "none";
+}
+
+function hideSkills() {
+  var page = document.getElementById("skills");
+  page.style.display = "none";
+}
+
+function hideLanguages() {
+  var page = document.getElementById("languages");
+  page.style.display = "none";
+}
+
+function hideProjects() {
+  var page = document.getElementById("projects");
+  page.style.display = "none";
 }
 
 showHome();
