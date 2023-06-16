c7ea8cf849694bfa51fbc88f19414562dc344550
Alexandra Bichir <alexandrabichir@yahoo.com>
2023-06-16 19:53:15 +0300

    add showPage with id param

--- index.js
+++ index.js
@@ -5,84 +5,19 @@ function hideAllPages() {
   document.getElementById("projects").style.display = "none";
 }
 
-function showHome() {
-  hideAllPages();
-  var page = document.getElementById("home");
-  page.style.display = "block";
-}
-
-function showSkills() {
-  hideAllPages();
-  var page = document.getElementById("skills");
-  page.style.display = "block";
-}
-
 function showLanguages() {
-  hideAllPages();
-  var page = document.getElementById("languages");
-  page.style.display = "block";
+  showPage("languages");
 }
 
 function showProjects() {
+  showPage("projects");
+}
+
+function showPage(id) {
   hideAllPages();
-  var page = document.getElementById("projects");
+  var page = document.getElementById(id);
+  console.info("show", page);
   page.style.display = "block";
 }
 
-//debugger;
-
-showHome();
-
-// function showHome() {
-//   var page = document.getElementById("home");
-//   page.style.display = "block";
-//   hideSkills();
-//   hideLanguages();
-//   hideProjects();
-// }
-
-// function showSkills() {
-//   var page = document.getElementById("skills");
-//   page.style.display = "block";
-//   hideHome();
-//   hideLanguages();
-//   hideProjects();
-// }
-
-// function showLanguages() {
-//   var page = document.getElementById("languages");
-//   page.style.display = "block";
-//   hideHome();
-//   hideSkills();
-//   hideProjects();
-// }
-
-// function showProjects() {
-//   var page = document.getElementById("projects");
-//   page.style.display = "block";
-//   hideHome();
-//   hideSkills();
-//   hideLanguages();
-// }
-
-// function hideHome() {
-//   var page = document.getElementById("home");
-//   page.style.display = "none";
-// }
-
-// function hideSkills() {
-//   var page = document.getElementById("skills");
-//   page.style.display = "none";
-// }
-
-// function hideLanguages() {
-//   var page = document.getElementById("languages");
-//   page.style.display = "none";
-// }
-
-// function hideProjects() {
-//   var page = document.getElementById("projects");
-//   page.style.display = "none";
-// }
-
-showHome();
+showPage("home");
